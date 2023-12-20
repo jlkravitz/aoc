c1e124ba505d92c5f7d40078b8312f11d7f37501
Joshua Kravitz <jlkravitz@gmail.com>
2023-12-19 20:55:28 -0500

    Make number more readable

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -94,7 +94,7 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    Some(Image::new(input).shortest_paths(1000000).sum())
+    Some(Image::new(input).shortest_paths(1_000_000).sum())
 }
 
 #[cfg(test)]
